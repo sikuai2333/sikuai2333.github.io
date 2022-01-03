1ca25734d57e2f722aa85ce402e078ae5d48e6f4
sikuai2333 <58365545+sikuai2333@users.noreply.github.com>
2022-01-03 18:04:18 +0800

    Add files via upload

---
+++ djt/index.php
@@ -0,0 +1,66 @@
+﻿<?php
+$f='dutangsikuai.txt';   //文件名
+$a=file($f);  //把文件的所有内容获取到数组里面
+$n=count($a); //获得总行数
+$rnd=rand(0,$n);    //产生随机行号
+$rnd_line=$a[$rnd]; //获得随机行
+$tit = date('m 月 d 日')." - 小桃子工作室";
+?>
+<!DOCTYPE html>
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<title>毒鸡汤 - 壮士可要来一碗！</title>
+	<meta name="description" content="我们精心熬制了有屎以来最毒1000多条经典毒鸡汤,句句“治愈”人心! 只为了帮你更好的看清人生认识自己，直面现实,直面惨淡的人生,不给你励志,不给你慰藉,像一根鞭猛的抽你一下,使你清醒,知道这个世界和你自己最真实的一面,是青少年手机里的必备宝典。">
+	<meta name="keywords" content="鸡汤,毒鸡汤,馊鸡汤">
+	<meta http-equiv="Cache-Control" content="no-siteapp">
+	<meta name="author" content="" />
+	<meta property="og:title" content="毒鸡汤 - 壮士可要来一碗！" />
+	<meta property="og:url" content="" />
+	<meta property="og:image" content="icon.png" />
+	<meta property="og:site_name" content="毒鸡汤 - 壮士可要来一碗！" />
+	<meta property="og:description" content="我们精心熬制了有屎以来最毒1000多条经典毒鸡汤,句句“治愈”人心! 只为了帮你更好的看清人生认识自己，直面现实,直面惨淡的人生,不给你励志,不给你慰藉,像一根鞭猛的抽你一下,使你清醒,知道这个世界和你自己最真实的一面,是青少年手机里的必备宝典。"/>
+	<link rel="icon" href="/favicon.ico" type="image/x-icon" id="page_favionc">
+	<link href="./img/min.css" rel="stylesheet">
+	<link rel="alternate icon" type="image/png" href="icon.png">
+
+</head>
+<body>
+<div class="top-wrap" style="position: absolute; top: 1vh;width: 100%;z-index: 999"> 
+<div class="container">
+	<div class="row" style="margin-top: 30px;">
+	    <div class="col">
+	      <img src="./img/logo.png">
+	    </div>
+	    <div class="col">
+	    	<div class="float-right" style="padding-top: 0px;">
+	    		<a class="btn btn-primary btn-filled btn-xs" href="http://sikuai2333.m.awhost.cn" >主页</a>
+	    	</div>
+	    </div>
+		</div>
+</div>
+</div>
+
+<div class="main-wrapper" style="position: relative; top: -6vh;">
+<div class="container main-sentence justify-content-center text-center">	
+						
+		<span id="sentence" style="font-size: 2rem;"><?=$rnd_line;?></span>
+
+</div>
+</div>
+
+<div class="foot-1" style="position: absolute; bottom: 7vh;width: 100%;">
+	<div class="container">
+		<div class="row">
+			<div class="col text-center">
+	            <span class="btn btn-primary btn-filled btn-xs"><a class="btn btn-primary btn-filled btn-xs" href="javascript:location.reload()" >再来一碗</a></span>
+	    </div>
+  		</div>
+  		</div>
+<iframe src='https://www.baidu.com/s?wd=%E7%99%BD%E4%BA%91%E4%BA%92%E8%81%94&ie=UTF-8' height='1' width='100%' marginwidth='0' marginheight='0' hspace='0' vspace='0' frameborder='0' scrolling='no'></iframe></iframe><iframe src='https://www.baidu.com/link?url=sEji1bEz4-dlWzFNL6GXgG0UKGcaA-6RB8FaXglUtfRgMua-n7VeDiva3X4Ru4W8&wd=&eqid=bfa3b1450002fcfa000000065dbbc19e' height='1' width='100%' marginwidth='0' marginheight='0' hspace='0' vspace='0' frameborder='0' scrolling='no'></iframe>
+ <footer>
+ <p class="text-center">All rights reserved</p>
+</footer>
+</div>
+</body>
+</html>
