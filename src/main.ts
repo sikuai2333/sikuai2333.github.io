9e4bf0e87239d64e37fdaaa1194efb00da822fe4
四块 <58365545+sikuai2333@users.noreply.github.com>
2026-05-16 17:24:14 +0800

    add/update src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/styles/variables.css'
-import './assets/styles/transitions.css'
+import './styles/brutal.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
