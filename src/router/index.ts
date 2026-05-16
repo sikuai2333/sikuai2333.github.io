da54920b8ceb3d89432386c09889ff640a9df546
四块 <58365545+sikuai2333@users.noreply.github.com>
2026-05-16 23:17:54 +0800

    Add src/router/index.ts

---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/HomePage.vue'),
+  },
+]
+
+export default createRouter({ history: createWebHistory(), routes })
