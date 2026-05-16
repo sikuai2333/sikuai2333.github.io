ef94fcc7cdc728ddd9803a2c6973012ea54429a9
四块 <58365545+sikuai2333@users.noreply.github.com>
2026-05-16 23:21:17 +0800

    Add src/views/HomePage.vue

---
+++ src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import HeroSection from '@/components/HeroSection.vue'
+import ToolsGrid from '@/components/ToolsGrid.vue'
+import FunGrid from '@/components/FunGrid.vue'
+import SitesNav from '@/components/SitesNav.vue'
+</script>
+
+<template>
+  <HeroSection />
+  <ToolsGrid />
+  <FunGrid />
+  <SitesNav />
+</template>
